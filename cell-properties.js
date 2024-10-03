64800253a32eb021da183e905f00c94dc0ed1c00
abhishek <abhishekbahukhandi2020@gmail.com>
2024-10-03 23:33:14 +0530

    more

---
+++ cell-properties.js
@@ -0,0 +1,26 @@
+//storage
+
+let sheetDB=[];
+
+for(let i=0;i<rows;i++){
+    let sheetRow=[];
+    for( let j=0; j<cols; j++){
+        let cellProp={
+            bold: false,
+            italic: false,
+            undeline: false,
+            alignment: "left",
+            fontFamily:"monospace",
+            fontColor: "#000000",
+            fontSize:"14",
+            fontColor :"#000000",
+            BGcolor:"#000000"
+
+
+        }
+        sheetRow.push(cellProp);
+         
+    }
+
+    sheetDB.push(sheetRow)
+}
